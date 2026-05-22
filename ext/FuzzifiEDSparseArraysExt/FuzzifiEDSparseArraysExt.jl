c7747204f40970e82b485868ce17878cebe1f842
matt.yusuf11@gmail.com <matt.yusuf11@gmail.com>
2026-05-22 11:26:08 +0100

    Performace Imrpovement for SparseMatrixCSC wrapper
    replaced generating all row, column and value coordinates with sorting
    the row indices in each column in place.

--- ext/FuzzifiEDSparseArraysExt/FuzzifiEDSparseArraysExt.jl
+++ ext/FuzzifiEDSparseArraysExt/FuzzifiEDSparseArraysExt.jl
@@ -6,6 +6,38 @@ using FuzzifiED
 
 import FuzzifiED: OpMat
 
+"""
+    canonicalize_csc!(colptr::Vector{Int}, rowval::Vector{Int}, nzval)
+
+Sorts the row indices and corresponding non-zero values in-place for each column of a sparse matrix in compressed sparse column (CSC) format. 
+This function ensures that the row indices within each column are in ascending order, which is expected by the `SparseMatrixCSC` constructor in the `SparseArrays` package. 
+The function takes three arguments: `colptr`, which is a vector of column pointers; `rowval`, which is a vector of row indices; and `nzval`, which is a vector of non-zero values corresponding to the row indices. 
+The sorting is performed for each column, and the function modifies the input vectors in-place.
+"""
+
+function canonicalize_csc!(
+    colptr::Vector{Int},
+    rowval::Vector{Int},
+    nzval
+)
+
+    n = length(colptr) - 1
+
+    for col in 1:n
+
+        r = colptr[col]:(colptr[col+1]-1)
+
+        # skip small columns
+        length(r) <= 1 && continue
+
+        p = sortperm(view(rowval, r))
+
+        rowval[r] = rowval[r][p]
+        nzval[r]  = nzval[r][p]
+
+    end
+end
+
 
 """
     SparseMatrixCSC(mat :: OpMat{ComplexF64}) :: SparseMatrixCSC{Int64,ComplexF64}
@@ -14,17 +46,10 @@ import FuzzifiED: OpMat
 converts the `OpMat` objects to a `SparseMatrixCSC` object in the `SparseArrays` package.
 """
 function SparseArrays.SparseMatrixCSC(mat :: OpMat)
-    I = Int[]
-    J = Int[]
-    V = eltype(mat.elval)[]
-    for col in 1:mat.dimd
-        for ptr in mat.colptr[col]:(mat.colptr[col+1]-1)
-            push!(I, mat.rowid[ptr])
-            push!(J, col)
-            push!(V, mat.elval[ptr])
-        end
-    end
-    matcsc1 = sparse(I, J, V, mat.dimf, mat.dimd)
+    rowval = copy(mat.rowid)
+    nzval = copy(mat.elval)
+    canonicalize_csc!(mat.colptr, rowval, nzval)
+    matcsc1 = SparseMatrixCSC(mat.dimd, mat.dimd, mat.colptr, rowval, nzval)
     if (mat.sym_q == 0) 
         return matcsc1
     elseif (mat.sym_q == 1)
